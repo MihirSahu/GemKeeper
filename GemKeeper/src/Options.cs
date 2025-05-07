df57c0d826fa467eff20e48c889115fdc331152e
Mihir Sahu <2002mihir@gmail.com>
2025-05-06 21:30:47 -0500

    feat: add basic options

---
+++ GemKeeper/src/Options.cs
@@ -0,0 +1,40 @@
+using CommandLine;
+using CommandLine.Text;
+
+namespace GemKeeper
+{
+  public class Options
+  {
+    [Usage(ApplicationAlias = "gemkeeper")]
+    public static IEnumerable<Example> Examples
+    {
+      get
+      {
+        return new List<Example>() {
+        new Example("Export Gemini chat history to markdown", new Options { FilePath = "./takeout.zip", OutputPath = "gemini_output/" })
+      };
+      }
+    }
+    [Option('f', "file-path", Required = true, HelpText = "Specify the activity file path.")]
+    public string? FilePath { get; set; }
+
+    [Option('o', "output", Required = true, HelpText = "Specify the output folder.")]
+    public string? OutputPath { get; set; }
+
+    [Option('v', "verbose", Required = false, HelpText = "Print additional logs.")]
+    public bool Verbose { get; set; }
+
+    public static void DisplayHelp<T>(ParserResult<T> result)
+    {
+      var helpText = HelpText.AutoBuild(result, h =>
+      {
+        h.AdditionalNewLineAfterOption = false;
+        h.Heading = "Gemkeeper v1.0.0";
+        h.Copyright = "Made with <3 by Mihir Sahu";
+        return HelpText.DefaultParsingErrorsHandler(result, h);
+      }, e => e);
+      Console.WriteLine(helpText);
+    }
+  };
+
+}
