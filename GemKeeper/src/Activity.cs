e34133a4dccac266e82b967a4b1f24877c3ef930
Mihir Sahu <2002mihir@gmail.com>
2025-05-08 12:39:54 -0500

    feat: add activity class for JSON deserialization

---
+++ GemKeeper/src/Activity.cs
@@ -0,0 +1,13 @@
+public class SafeHtmlItem {
+  public string html {get; set;}
+}
+
+class Activity
+{
+  public string header {get; set;}
+  public string title {get; set;}
+  public string time {get; set;}
+  public string[] products {get; set;}
+  public string[] activityControls {get; set;}
+  public List<SafeHtmlItem> safeHtmlItem {get; set;}
+}
